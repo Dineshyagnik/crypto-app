5f259e8bff58537dc9dd136d73387551a9ef34c4
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-06-15 21:36:04 +0530

    add chart

--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -1,48 +1,56 @@
-import React from 'react';
-import {Line} from 'react-chartjs-2';
+import React from "react";
+import { Line } from "react-chartjs-2";
 import {
-    Chart as chartjs,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-} from 'chart.js';
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 
-chartjs.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-)
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
-const chart = ({arr=[],currency,days}) => {
+const Chart = ({ arr = [], currency, days }) => {
+  const prices = [];
+  const date = [];
 
-    const prices=[1,2,34];
-    const date=['12/2/22','23/2/23','32/2/33'];
-    const data={
+  for (let i = 0; i < arr.length; i++) {
+    if (days === "24h") date.push(new Date(arr[i][0]).toLocaleTimeString());
+    else date.push(new Date(arr[i][0]).toLocaleDateString());
+    prices.push(arr[i][1]);
+  }
 
-    }
+  const data = {
+    labels: date,
+    datasets: [
+      {
+        label: `Price in ${currency}`,
+        data: prices,
+        borderColor: "rgb(255,99,132)",
+        backgroundColor: "rgba(255,99,132,0.5)",
+      },
+    ],
+  };
 
   return (
-    <Line options={{
-        responsive:true,
-    }}
-    data={{
-        labels:date,
-        datasets:[{
-            label:`Price in ${currency}`,
-            data:prices,borderColor:'rgb(255,99,132)',
-            backgroundColor:'rgb(255,99,132,0.5)',
-        }]
-    }}
+    <Line
+      options={{
+        responsive: true,
+      }}
+      data={data}
     />
-  )
-}
+  );
+};
 
-export default chart;
+export default Chart;
